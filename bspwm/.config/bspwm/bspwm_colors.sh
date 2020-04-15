81b2f4c40af023177cfa87785ff87049442cfad3
Kelly Crabbé <kelly.crabbe@gmail.com>
2020-04-16 00:04:10 +0200

    update

--- bspwm/.config/bspwm/bspwm_colors.sh
+++ bspwm/.config/bspwm/bspwm_colors.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#774f21"
-bspc config normal_border_color  "#523617"
+bspc config focused_border_color "$(xrdb -query | grep 'color4:'| awk '{print $NF}')"
+bspc config normal_border_color "$(xrdb -query | grep 'color0:'| awk '{print $NF}')"
+bspc config active_border_color "$(xrdb -query | grep 'color5:'| awk '{print $NF}')"
+bspc config presel_feedback_color "$(xrdb -query | grep 'color2:'| awk '{print $NF}')"
+
