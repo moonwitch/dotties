b6912c3472d17bf976b9df5a696bf276ccfe2ee3
Kelly Crabbé <kelly.crabbe@gmail.com>
2020-06-04 14:25:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ nerd-fonts-fira-mono \
 nerd-fonts-jetbrains-mono \
 ttf-comfortaa \
 python-pip \
-numlockx xidlehook \
+numlockx \
 zathura neofetch \
 sni-qt \
 stow \
@@ -29,12 +29,14 @@ boston-icon-theme-git \
 arc-icon-theme-git \
 oranchello-icon-theme-git \
 paper-icon-theme-git \
+/
 
 # Work installs
 baph -iNn networkmanager-openconnect \
 pidgin \
 mattermost-desktop-bin \
 bitwarden-bin-git \
+rbw \
 firefox \
 /
 
@@ -56,23 +58,26 @@ curl -L https://raw.githubusercontent.com/dikiaap/dotfiles/master/.oh-my-zsh/the
 pip install --global --upgrade oauth2client google-api-python-client
 
 # Install lightdm looker
-systemctl enable betterlockscreen@$USER
-# baph -inN lightdm-webkit-theme-litarvan lightdm-webkit-theme-aether
+# systemctl enable betterlockscreen@$USER
+baph -inN lightdm-webkit-theme-aether
 
 # Fix Grub2
-echo "GRUB_GFXMODE=3840x2160x32" >> /etc/default/grub
+echo "GRUB_GFXMODE=3840x2160x24" >> /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Generate my ssh key for github/bitbucket
-ssh-keygen -t rsa -b 4096 -C "kelly.crabbe@gmail.com"
+# ssh-keygen -t rsa -b 4096 -C "kelly.crabbe@gmail.com"
 
 # Services
 # systemctl --user start spotifyd.service
 # systemctl --user enable spotifyd.service
 systemctl start systemd-timesyncd
 systemctl enable systemd-timesynd
+
+# Shares
 sudo mkdir -p /mnt/work/{home,buo,portimaprod,portimaqual,spotlight}
 sudo mkdir -p /mnt/NAS/{media,photos,software}
 #sudo mount.cifs -o username=intamv\\700320 //fci.fortis/amv/buo /mnt/work/buo
 #sudo mount -t cifs -o username=intamv\\700320 //fci.fortis/acidfs01/brulthome/700320 /mnt/work/home
-echo "UUID=$(lsblk -no UUID /dev/sdb1) /media/Storage $(lsblk -no FSTYPE /dev/sdb1) defaults,noatime 0 2" >> /etc/fstab
+sudo mkdir -p /mnt/storage
+echo "UUID=$(lsblk -no UUID /dev/sdb1) /mnt/storage $(lsblk -no FSTYPE /dev/sdb1) defaults,noatime 0 2" >> /etc/fstab
