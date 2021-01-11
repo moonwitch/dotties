bbb4d1b0341e064b543b96201443e5a7f52b20a5
Kelly Crabbé <kelly.crabbe@gmail.com>
2021-01-11 14:20:03 +0100

    Adding some completions and further enhancing fish

--- settings/.config/fish/config.fish
+++ settings/.config/fish/config.fish
@@ -1,8 +1,15 @@
 # Set Greetings
 set -g -x fish_greeting 'Welcome home!'
 
-# Ensure my aliases are loaded
-source .aliases
+# Ensure I've got aliases
+if test -e ~/.aliases
+  source ~/.aliases
+end
+
+# NPM with no sudo
+set NPM_PACKAGES "$HOME/.npm-packages"
+export PATH="$PATH:$NPM_PACKAGES/bin"
 
 # Prompt!
 starship init fish | source
+
