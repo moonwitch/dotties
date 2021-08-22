e6e37aea1d65b0aca05c37455637fb70a47f45b8
Kelly Crabbé <kelly.crabbe@gmail.com>
2021-08-22 21:50:15 +0200

    update for laptop

--- bspwm/.config/bspwm/bspwm_colors.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-bspc config focused_border_color "$(xrdb -query | grep 'color4:'| awk '{print $NF}')"
-bspc config normal_border_color "$(xrdb -query | grep 'color0:'| awk '{print $NF}')"
-bspc config active_border_color "$(xrdb -query | grep 'color5:'| awk '{print $NF}')"
-bspc config presel_feedback_color "$(xrdb -query | grep 'color2:'| awk '{print $NF}')"
-
